5c525b6750cc6f2b941a7eb7259c1543243edb05
Fernando Bahamondes <ferbaco@gmail.com>
2020-08-19 18:34:36 -0400

    Add more styling to the form

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
+function render(books) {
+  for (let book = 0; book < books.length; book += 1) {
+    const
+      {
+        title, author, pages, readStatus 
+      } = books[book];
+    const container = document.getElementById('bookcontainer');
+    const card = document.createElement('div');
+    card.classList.add('card', 'column', 'is-4');
+    container.append(card);
+    const bookTitle = document.createElement('h1');
+    bookTitle.classList.add('title', 'is-3');
+    bookTitle.innerHTML = title;
+    card.append(title);
+  }
+}
+
 function addBookToLibrary() {
   const newTitle = document.getElementById('title').value;
   const newAuthor = document.getElementById('author').value;
@@ -16,24 +33,8 @@ function addBookToLibrary() {
 
   const newBook = new Book(newTitle, newAuthor, newPages, newReadStatus);
   myLibrary.push(newBook);
-  document.getElementById("addbook").reset()
-  render(myLibrary)
+  document.getElementById('addbook').reset();
+  render(myLibrary);
 }
 
 addBook.addEventListener('click', addBookToLibrary);
-
-function render(books) {
-    for (let book = 0; book < books.length; book++){ 
-        const {
-            title, author, pages, readStatus 
-        } = books[book];
-        const container = document.getElementById('bookcontainer');
-        const card = document.createElement('div');
-        card.classList.add('card', 'column' , 'is-4');
-        container.append(card);
-       const bookTitle = document.createElement('h1');
-       bookTitle.classList.add('title', 'is-3');
-       bookTitle.innerHTML = title;
-       card.append(title);
-    }
-}
