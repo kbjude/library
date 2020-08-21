4b686a30eb345471147ae4664697648e6db9f890
Fernando Bahamondes <ferbaco@gmail.com>
2020-08-21 00:41:18 -0400

    Update read status with toggle slider. Update style of the card. Update button text on show form

--- index.js
+++ index.js
@@ -9,19 +9,14 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
-// function deleteBook(button) {
-//   const bookIndex = button.getAttribute('data-index');
-//   myLibrary.splice(bookIndex, 1);
-
-// }
-
 function render(books) {
   const lastBookIndex = myLibrary.length - 1;
   for (let book = lastBookIndex; book < books.length; book += 1) {
     const
       {
-        title, author, pages, readStatus 
+        title, author, pages, readStatus,
       } = books[book];
+
     // Create elements
 
     const container = document.getElementById('bookcontainer');
@@ -31,9 +26,13 @@ function render(books) {
     const bookAuthor = document.createElement('h3');
     const bookPages = document.createElement('h4');
     const bookReadStatus = document.createElement('h4');
-    const readStatusText = document.createElement('h4');
+    const readSwitchLabel = document.createElement('label');
+    const readSwitchInput = document.createElement('input');
+    const readSwitchSpan = document.createElement('span');
     const deleteBookBtn = document.createElement('button');
+
     card.setAttribute('data-index', `${book}`);
+    readSwitchInput.type = 'checkbox';
 
     deleteBookBtn.addEventListener('click', (e) => {
       const bookIndex = card.getAttribute('data-index');
@@ -45,29 +44,31 @@ function render(books) {
     // Class additions
 
     card.classList.add('card', 'column', 'is-4');
-    cardContent.classList.add('card-content');
-    bookAuthor.classList.add('title', 'is-5', 'has-text-centered');
+    cardContent.classList.add('card-content', 'has-text-centered');
+    bookAuthor.classList.add('title', 'is-5');
     bookTitle.classList.add('title', 'is-3', 'box');
-    bookPages.classList.add('title', 'is-6', 'has-text-centered');
-    bookReadStatus.classList.add('title', 'is-6', 'has-text-centered');
-    readStatusText.classList.add('title', 'is-6', 'has-text-centered');
-    deleteBookBtn.classList.add('button', 'is-danger');
+    bookPages.classList.add('title', 'is-6');
+    bookReadStatus.classList.add('title', 'is-6');
+    readSwitchSpan.classList.add('slider', 'round');
+    readSwitchLabel.classList.add('switch', 'm-auto');
+    deleteBookBtn.classList.add('button', 'is-danger', 'mt-20');
 
     // Inner HTML declarations
 
     bookTitle.innerHTML = title;
     bookAuthor.innerHTML = author;
     bookPages.innerHTML = `${pages} pages`;
-    bookReadStatus.innerHTML = readStatus;
-    readStatusText.innerHTML = 'Already read?';
+    bookReadStatus.innerHTML = 'Have you read it yet?';
     deleteBookBtn.innerHTML = 'Delete Book';
+    readSwitchInput.checked = readStatus;
 
     // Appends
 
     container.append(card);
     card.append(cardContent);
+    readSwitchLabel.append(readSwitchInput, readSwitchSpan);
     cardContent.append(bookTitle, bookAuthor,
-      bookPages, readStatusText, bookReadStatus, deleteBookBtn);
+      bookPages, bookReadStatus, readSwitchLabel, deleteBookBtn);
   }
 }
 
@@ -76,7 +77,7 @@ function addBookToLibrary() {
   const newTitle = document.getElementById('title').value;
   const newAuthor = document.getElementById('author').value;
   const newPages = document.getElementById('pages').value;
-  const newReadStatus = document.getElementById('readStatus').value;
+  const newReadStatus = document.getElementById('readStatus').checked;
 
   const newBook = new Book(newTitle, newAuthor, newPages, newReadStatus);
   myLibrary.push(newBook);
@@ -86,7 +87,13 @@ function addBookToLibrary() {
 
 function showForm() {
   const form = document.getElementById('addbook');
+  const button = document.getElementById('show-form-btn');
   form.classList.toggle('is-hidden');
+  if (button.innerHTML === 'Show Form') {
+    button.innerHTML = 'Hide Form';
+  } else {
+    button.innerHTML = 'Show Form';
+  }
 }
 
 addBook.addEventListener('click', addBookToLibrary);
